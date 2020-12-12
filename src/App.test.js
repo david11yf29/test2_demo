30e7fff1b516516a13cdad899a2b8eb4f27f073e
david11yf29 <david11yf29@gmail.com>
2020-12-12 18:22:56 +0800

    updated

--- src/App.test.js
+++ src/App.test.js
@@ -9,4 +9,5 @@ Enzyme.configure({ adapter: new Adapter() });
 test('renders without crashing', () => {
   const wrapper = shallow(<App />);
   console.log(wrapper.debug());
+  expect(wrapper).toBeFalsy();
 });
