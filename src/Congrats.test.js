aeab97653933cc2a1c5ba94798678016aeed7d5a
david11yf29 <david11yf29@gmail.com>
2020-12-13 09:31:00 +0800

    updated

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -12,14 +12,20 @@ const setup = (props={}) => {
 }
 
 test('renders without error', () => {
-
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, 'component-congrats');
+  expect(component.length).toBe(1);
 });
 
 test('renders no text when `success` prop is false', () => {
-
+  const wrapper = setup({ success: false });
+  const component = findByTestAttr(wrapper, 'component-congrats');
+  expect(component.text()).toBe('');
 });
 
 test('renders non-empty congrats message when success prop is true', () => {
-
+  const wrapper = setup({ success: true });
+  const messgae = findByTestAttr(wrapper, 'component-message');
+  expect(messgae.text().length).not.toBe(0);
 });
 
