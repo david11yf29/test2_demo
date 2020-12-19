71765387744086bb3f2b68167a8fb1fa7e69be47
david11yf29 <david11yf29@gmail.com>
2020-12-19 13:45:08 +0800

    updated

--- src/integration.test.js
+++ src/integration.test.js
@@ -2,9 +2,26 @@ import { storeFactory } from '../test/testUtils';
 import { guessWord } from './actions/index';
 
 describe('guessWord action dispatcher', () => {
+  const secretWord = 'party';
+  const unsuccessfulGuess = 'train';
   describe('no guess words', () => {
+    let store;
+    const initialState = { secretWord: secretWord }
+    beforeEach(() => {
+      store = storeFactory(initialState);
+    })
     test('updates state correctly for unsuccessful guess', () => {
-
+      store.dispatch(guessWord(unsuccessfulGuess));
+      const newState = store.getState()
+      const expectedState = { 
+        ...initialState, 
+        success: false,
+        guessedWords: [{ 
+          guessedWord: unsuccessfulGuess,
+          letterMatchCount: 3
+         }]
+      };
+      expect(newState).toEqual(expectedState);
     });
     test('updates state correctly for successful guess', () => {
 
