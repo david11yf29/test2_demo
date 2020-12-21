a1621014663b72609c73e638b0054a6a93c87463
david11yf29 <david11yf29@gmail.com>
2020-12-21 18:19:19 +0800

    updated

--- src/App.test.js
+++ src/App.test.js
@@ -11,5 +11,27 @@ const setup = (state = {}) => {
 };
 
 describe('redux properties', () => {
-  
-})
+  test('has access to `success` state', () => {
+    const success = true;
+    const wrapper = setup({ success: success });
+    const successProp = wrapper.instance().props.success;
+    expect(successProp).toBe(success);
+  });
+  test('has access to `secretWord` state', () => {
+    const secretWord = 'party';
+    const wrapper = setup({ secretWord: secretWord });
+    const secretWordProp = wrapper.instance().props.secretWord;
+    expect(secretWordProp).toBe(secretWord);
+  });
+  test('has access to `guessedWords` state', () => {
+    const guessedWords = [{ guessedWord: 'train', letterMatchCount: 3 }];
+    const wrapper = setup({ guessedWords: guessedWords });
+    const guessedWordsProp = wrapper.instance().props.guessedWords;
+    expect(guessedWordsProp).toBe(guessedWords);
+  });
+  test('`getSecretWord` action creator is a function on the props', () => {
+    const wrapper = setup();
+    const getSecretWordProp = wrapper.instance().props.getSecretWord;
+    expect(getSecretWordProp).toBeInstanceOf(Function);
+  });
+});
